384e6342a28dc04ea7010bd1ffef25a87096dd33
nomura-ki <nomura.ki@tdc.co.jp>
2025-11-21 10:02:16 +0900

    fix display JSXのattribute修正

--- src/components/display/SubDisplay.jsx
+++ src/components/display/SubDisplay.jsx
@@ -1,7 +1,7 @@
 export default function SubDisplay({ value }) {
   const subDis = value;
   return (
-    <div class="text-[25px] text-right whitespace-nowrap overflow-hidden">
+    <div className="text-[25px] text-right whitespace-nowrap overflow-hidden">
       {subDis}
     </div>
   );
